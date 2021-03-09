57e58c786bb0a76c64a223392ac4da1f9eeaea52
Viktor Loshchev <75720631+viklosh@users.noreply.github.com>
2021-03-09 20:49:15 +0300

    Terraform and ansible
    * Feature/add yandex compute instances (#1)
    
    * feat-declare yc variables
    
    * feat-create two vm ubuntu 20.04
    
    Co-authored-by: Viktor Loshchev <viktor.loshhev@simbirsoft.com>
    
    * Feature/add provisioners (#2)
    
    * feat-add docker file
    
    * feat-provisioners for docker deploy
    
    * feat-ignore files
    
    Co-authored-by: Viktor Loshchev <viktor.loshhev@simbirsoft.com>
    
    * Feature/ansible provision
    
    * feat-add ansible cfg file
    
    * feat-add build playbook
    
    * feat-add deploy playbook
    
    * feat-change provisioners to ansible
    
    Co-authored-by: Viktor Loshchev <viktor.loshhev@simbirsoft.com>
    
    Co-authored-by: Viktor Loshchev <viktor.loshhev@simbirsoft.com>

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ terraform {
       source = "yandex-cloud/yandex"
       version = "0.51.1"
     }
-    # docker = {
-    #   source = "kreuzwerker/docker"
-    #   version = "2.11.0"
-    # }
   }
 }
 
@@ -17,16 +13,6 @@ provider "yandex" {
   folder_id                = var.folder_id
   zone                     = "ru-central1-a"
 }
-
-# provider "docker" {
-#   alias  = "bulder"
-#   host = "tcp://${yandex_compute_instance.vm-1.network_interface.0.nat_ip_address}:2376/"
-# }
-
-# provider "docker" {
-#   alias  = "deployer"
-#   host = "tcp://${yandex_compute_instance.vm-2.network_interface.0.nat_ip_address}:2376/"
-# }
 
 resource "yandex_compute_instance" "vm-1" {
   name = "terraform1"
@@ -52,41 +38,8 @@ resource "yandex_compute_instance" "vm-1" {
     ssh-keys = "ubuntu:${file("./id_rsa.pub")}"
   }
 
-  provisioner "file" {
-    source      = "./Dockerfile"
-    destination = "~/Dockerfile"
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = file("./id_rsa")
-      host     = self.network_interface.0.nat_ip_address
-    }
-  }
-
-  provisioner "file" {
-    source      = "./key.json"
-    destination = "~/key.json"
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = file("./id_rsa")
-      host     = self.network_interface.0.nat_ip_address
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update && sudo apt install docker.io -y",
-      "cat key.json | sudo docker login --username json_key --password-stdin cr.yandex",
-      "sudo docker build -t cr.yandex/crp789tmi24lkfp3ieba/boxfuse .",
-      "sudo docker push cr.yandex/crp789tmi24lkfp3ieba/boxfuse"
-    ]
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = file("./id_rsa")
-      host     = self.network_interface.0.nat_ip_address
-    }
+  provisioner "local-exec" {
+    command = "ansible-playbook ansible-build.yaml -e 'hostname=${self.network_interface.0.nat_ip_address}'"
   }
 
   depends_on = [
@@ -117,33 +70,14 @@ resource "yandex_compute_instance" "vm-2" {
   metadata = {
     ssh-keys = "ubuntu:${file("./id_rsa.pub")}"
   }
-  
-  provisioner "file" {
-    source      = "./key.json"
-    destination = "~/key.json"
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = file("./id_rsa")
-      host     = self.network_interface.0.nat_ip_address
-    }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook ansible-deploy.yaml -e 'hostname=${self.network_interface.0.nat_ip_address}'"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update && sudo apt install docker.io -y",
-      "cat key.json | sudo docker login --username json_key --password-stdin cr.yandex",
-      "sudo docker run -d -p 8080:8080 cr.yandex/crp789tmi24lkfp3ieba/boxfuse"
-    ]
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = file("./id_rsa")
-      host     = self.network_interface.0.nat_ip_address
-    }
-  }
   depends_on = [
     yandex_vpc_network.network-1,
+    yandex_compute_instance.vm-1,
   ]
 }
 
@@ -156,17 +90,3 @@ resource "yandex_vpc_subnet" "subnet-1" {
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-
-# resource "docker_registry_image" "build_image" {
-#   provider = docker.bulder
-#   name = "cr.yandex/crp789tmi24lkfp3ieba/boxfuse"
-#   build {
-#     context = "."
-#     remote_context = "https://github.com/viklosh/ds-ex14.git"
-#     auth_config {
-#       host_name = "cr.yandex"
-#       user_name = "json_key"
-#       password = file("./key.json")
-#     }
-#   }
-# }
